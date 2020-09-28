09c053e027f3af357c612a3a898a2168ded6b66a
Matej Koncal <matej.koncal@resco.net>
2020-09-28 14:39:27 +0200

    fix

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const WebSocket = require('ws');
 const fs = require('fs');
-const https = require('http');
-const options = {
-	key: fs.readFileSync('key.pem'),
-	cert: fs.readFileSync('cert.pem'),
-};
-
-// myIP = require('my-ip');
-// console.dir(`Chat is running on https://${myIP()}/`);
+const http = require('http');
+// const options = {
+// 	key: fs.readFileSync('key.pem'),
+// 	cert: fs.readFileSync('cert.pem'),
+// };
 
 var server = http
 	.createServer((req, resp) => {
